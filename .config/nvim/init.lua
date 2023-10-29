f2bb6b6bf317eb245333ef1f667fc2f3a0face1d
vik <77307545+amuseee@users.noreply.github.com>
2023-10-29 13:05:10 -0500

    Update init.lua
    kanagawa colorscheme, bufferline theme, lualine theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- complete minimal nvim config for competitive coding
 
 -- vim commands
+vim.opt.termguicolors = true
 vim.wo.number = true
 vim.cmd("syntax on")
 vim.opt.clipboard = 'unnamedplus'
@@ -15,20 +16,87 @@ vim.cmd("autocmd BufNewFile *.cpp 0r ~/.config/nvim/skeleton.cpp")
 -- plugins & colorscheme (using vim-plug)
 vim.cmd("source $HOME/.config/nvim/vim-plug/plugins.vim")
 
-require("leaf").setup({
-    underlineStyle = "undercurl",
-    commentStyle = "italic",
-    functionStyle = "NONE",
-    keywordStyle = "italic",
-    statementStyle = "bold",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
-    transparent = false,
-    colors = { darkest = '#000000' },
-    overrides = {},
-    theme = "dark", -- default, based on vim.o.background, alternatives: "light", "dark"
-    contrast = "high", -- default, alternatives: "medium", "high"
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = true,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+		theme = { wave = {}, lotus = {}, dragon = {}, all = {
+			ui = {
+				bg_gutter = "none"
+			}
+		} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
 })
 
-vim.cmd("colorscheme leaf")
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa")
 
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+    refresh = {
+        statusline = 200,
+        tabline = 500,
+        winbar = 300,
+    }
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = { 'fileformat' },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
+
+require("bufferline").setup{}
