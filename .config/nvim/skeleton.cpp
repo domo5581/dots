820c7b23fefe889ae64b0d856fd35f970faf9cee
vik <77307545+amuseee@users.noreply.github.com>
2023-10-29 13:07:04 -0500

    better competitve programming template

--- .config/nvim/skeleton.cpp
+++ .config/nvim/skeleton.cpp
@@ -1,10 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//defines
+#define ll long long
+#define vi vector<int>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+
+void solve() {
+
+
+
+
+
+
+}
+
 int main() {
-
-
-
-
-
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    solve();
+    /*
+    int n; cin >> n;
+    for (int i=0; i<n; i++) {
+        solve();
+    }
+    */
 }
